0d0524de57c8a955a4c4b21614387ed0d3bbd4aa
Clayton Lan <clayton.dev21@gmail.com>
2021-10-01 14:01:08 +1300

    implement favourite button

--- skywalkpedia/components/buttons/FavoriteButton/FavoriteButton.js
+++ skywalkpedia/components/buttons/FavoriteButton/FavoriteButton.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import OutlineStar from "../../../assets/svg/OutlineStar.svg";
 import SolidStar from "../../../assets/svg/SolidStar.svg";
+import styles from "./FavoriteButton.module.scss";
 
 const FavoriteButton = () => {
+  const [active, setActive] = useState(false);
+
+  const handleButtonOnClick = () => {
+    setActive(!active);
+  };
   return (
-    <div>
-      <button>
-        {" "}
-        hello
-        <SolidStar />
-      </button>
-    </div>
+    <button className={styles.button} onClick={handleButtonOnClick}>
+      {active ? <SolidStar /> : <OutlineStar />}
+    </button>
   );
 };
 
