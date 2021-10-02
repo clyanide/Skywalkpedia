7e1b7720efdfed25b43422784c7a97f3b59814d7
Clayton Lan <clayton.dev21@gmail.com>
2021-10-03 12:18:53 +1300

    combine components

--- skywalkpedia/components/data-display/FilmCard/FilmCard.js
+++ skywalkpedia/components/data-display/FilmCard/FilmCard.js
@@ -2,26 +2,14 @@ import React from "react";
 import { FavoriteButton } from "../../buttons";
 import styles from "./FilmCard.module.scss";
 
-const FilmCard = () => {
+const FilmCard = (props) => {
   return (
     <div className={styles.card}>
       <div className={styles.heading}>
-        <h3>Episode 5 - Empire Strikes Back</h3>
+        <h3>{props.heading}</h3>
         <FavoriteButton variant="negative" size={25} />
       </div>
-      <p className={styles.description}>
-        long ago in a universe far far away... long ago in a universe far far
-        away... long ago in a universe far far away... long ago in a universe
-        far far away... long ago in a universe far far away... long ago in a
-        universe far far away... long ago in a universe far far away... long ago
-        in a universe far far away... long ago in a universe far far away...
-        long ago in a universe far far away... long ago in a universe far far
-        away... long ago in a universe far far away... long ago in a universe
-        far far away... long ago in a universe far far away... long ago in a
-        universe far far away... long ago in a universe far far away... long ago
-        in a universe far far away... long ago in a universe far far away...
-        long ago in a universe far far away...
-      </p>
+      <p className={styles.description}>{props.description}</p>
     </div>
   );
 };
--- skywalkpedia/pages/films/index.js
+++ skywalkpedia/pages/films/index.js
@@ -3,9 +3,26 @@ import { FavoriteButton, ChevronButton } from "../../components/buttons";
 import { HomeHeader, FilmHeader } from "../../components/headers";
 import { Search } from "../../components/input";
 import { FilmCard, PinBoard } from "../../components/data-display";
+import styles from "../../styles/Home.module.scss";
 
 const Home = ({ films }) => {
-  return <PinBoard />;
+  return (
+    <div className={styles.page}>
+      <HomeHeader />
+      <div className={styles.search}>
+        <Search />
+      </div>
+      <div className={styles.list}>
+        {films.results.map((film, index) => (
+          <FilmCard
+            heading={film.title}
+            description={film.opening_crawl}
+            key={index}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export const getStaticProps = async () => {
